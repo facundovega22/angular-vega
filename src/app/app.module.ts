5b4abbe8d20e2f959fc2b8e3afa0dde04cf9524e
facundo vega <facundovega022@gmail.com>
2022-09-22 22:29:40 -0300

    clase 2 - segundo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { NavidadComponent } from './components/navidad/navidad.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ToolbarComponent,
+    NavidadComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/navidad/navidad.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navidad',
+  templateUrl: './navidad.component.html',
+  styleUrls: ['./navidad.component.css']
+})
+export class NavidadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
